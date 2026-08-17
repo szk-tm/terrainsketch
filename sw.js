c2be88c5721a059174d6e173f4971cc6b80fcf18
szk-tm <b1002@n-koei.co.jp>
2026-08-17 10:02:06 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -16,7 +16,7 @@
  */
 'use strict';
 
-var VER   = '0.63';                        // ← 本体 APPVER と一致させること（build.py が検証）
+var VER   = '0.64';                        // ← 本体 APPVER と一致させること（build.py が検証）
 var CACHE = 'terrainsketch-v' + VER;
 
 var CORE_REQUIRED = './index.html';        // これが取れなければ install を失敗させる
